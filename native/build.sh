6d5d1433176ec2230bb15a41089ece5e13291985
httpdigest <kburjack@googlemail.com>
2020-05-23 20:36:46 +0200

    Output disassembly on JNI build

--- native/build.sh
+++ native/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Building JNI..."
 gcc -c -march=native -mtune=native -o build/JNI.o -fPIC -m64 -Ofast -I"${JAVA_HOME}/include" -I"${JAVA_HOME}/include/linux" src/JNI.c -lm
-gcc -c -march=native -mtune=native -o build/bench_Matrix4fn.o -fPIC -m64 -Ofast -I"${JAVA_HOME}/include" -I"${JAVA_HOME}/include/linux" src/bench_Matrix4fn.c -lm
+gcc -c -save-temps -march=native -mtune=native -o build/bench_Matrix4fn.o -fPIC -m64 -Ofast -I"${JAVA_HOME}/include" -I"${JAVA_HOME}/include/linux" src/bench_Matrix4fn.c -lm
 gcc -shared -static-libgcc -O3 -o build/libjoml.so -fPIC -m64 build/JNI.o build/bench_Matrix4fn.o -lm
+cat bench_Matrix4fn.s
 strip -x -s build/libjoml.so
